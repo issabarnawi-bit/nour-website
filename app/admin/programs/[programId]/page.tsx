4e7c0325e5614702588f27da21a78d1db04f252e
issabarnawi-bit <issabarnawi@gmail.com>
2026-08-25 01:24:09 +0300

    feat(admin): link structured program content editor

--- app/admin/programs/[programId]/page.tsx
+++ app/admin/programs/[programId]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import ProgramDetailsPage from "../../../../src/features/programs/ProgramDetailsPage";
 
 type PageProps = {
@@ -12,8 +13,27 @@ export default async function Page({
   const { programId } = await params;
 
   return (
-    <ProgramDetailsPage
-      programId={programId}
-    />
+    <>
+      <div style={{ display: "flex", justifyContent: "flex-end", padding: "18px 24px 0" }}>
+        <Link
+          href={`/admin/programs/${programId}/content`}
+          style={{
+            display: "inline-flex",
+            alignItems: "center",
+            minHeight: 40,
+            padding: "0 14px",
+            borderRadius: 10,
+            color: "#fff",
+            background: "#176fe8",
+            textDecoration: "none",
+            fontSize: 13,
+            fontWeight: 800,
+          }}
+        >
+          إدارة محتوى تفاصيل البرنامج
+        </Link>
+      </div>
+      <ProgramDetailsPage programId={programId} />
+    </>
   );
 }
